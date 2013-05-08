148656fdbb4a4e6ba21f8454397222d73bd6c276
ayezutov <alexander.yezutov@gmail.com>
2013-05-08 11:09:10 -0400

    Created base UI

---
+++ Pipeliner/prototype/scripts/pipeliner.data.js
@@ -0,0 +1,29 @@
+﻿window.pipelinerData = {
+    pipelines: [
+        {
+            id: 1,
+            parameters: {
+                changeSet: 40010,
+            },
+            steps: [
+                {
+                    id: 1,
+                    status: 'success',
+                    transitionType: 'auto'
+                },
+                {
+                    id: 2,
+                    status: 'success'
+                },
+                {
+                    id: 3,
+                    status: 'success'
+                },
+                {
+                    id: 4,
+                    status: 'success'
+                }
+            ]
+        }
+    ]
+}
